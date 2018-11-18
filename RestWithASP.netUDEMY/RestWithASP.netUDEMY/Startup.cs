4ac9cf558a3f918d24acafc9f9d1944226514cfd
Rodrigo Salvate Brasil <rodrigosalvate@hotmail.com>
2018-11-18 19:06:51 -0200

    Developing database integration

--- RestWithASP.netUDEMY/RestWithASP.netUDEMY/Startup.cs
+++ RestWithASP.netUDEMY/RestWithASP.netUDEMY/Startup.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using RestWithASP.netUDEMY.Model.Context;
+using RestWithASP.netUDEMY.Services;
+using RestWithASP.netUDEMY.Services.Implementattions;
 
 namespace RestWithASP.netUDEMY
 {
@@ -24,7 +22,15 @@ namespace RestWithASP.netUDEMY
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            var connection = Configuration["MySqlConnection:MySqlConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+
+            services.AddMvc();
+
+            services.AddApiVersioning();
+
+            //Dependency Injection
+            services.AddScoped<IPersonService, PersonServiceImpl>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
